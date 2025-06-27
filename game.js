f0f2bde2ec29f0ea859fa21697e04d2f9124f295
PRIME8s <prime8sape@protonmail.com>
2025-06-26 22:28:48 -0700

    Update game.js

--- game.js
+++ game.js
@@ -15,7 +15,7 @@ const jumpStrength = -10;
 let keys = {};
 let barrels = [];
 let platforms = [
-  { x: 0, y: 600, width: 480, height: 20 },
+  { x: 0, y: 600, width: 480, height: 10 },
   { x: 0, y: 500, width: 400, height: 10 },
   { x: 80, y: 400, width: 400, height: 10 },
   { x: 0, y: 300, width: 400, height: 10 },
